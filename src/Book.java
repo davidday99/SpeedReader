b6ecd9b3491b9202e7dee81773d50b1b22e393e5
David <david.day@utexas.edu>
2019-06-06 19:38:30 -0500

    Timer class created with methods for measuring time elapsed, JavaDoc comments added to Book and Timer classes

--- src/Book.java
+++ src/Book.java
@@ -6,25 +6,16 @@ import java.util.Scanner;
 
 public class Book {
     public String title;
-    private String entireText;
-    private ArrayList<String> words;
-    private int currentWordIndex;
+    private String entireText;    // string containing full text
+    private ArrayList<String> words;    // array containing each whitespace-separated word
+    private int currentWordIndex;    // index set to currently viewed word in the arraylist
 
-    public static void main(String[] args) throws Exception {
-        Book b1 = new Book("text.txt");
-        System.out.println(b1.getWordAtCurrentIndex());
-        System.out.println(b1.getNextWord());
-        System.out.println(b1.getNextWord());
-        System.out.println(b1.getPreviousWord());
-        b1.printWords();
-    }
-
-    public Book(String bookTitle) throws FileNotFoundException {
+    public Book(String bookTitle) {
         title = bookTitle;
         currentWordIndex = 0;
 
         try {
-            entireText = new Scanner(new File(title)).useDelimiter("\\A").next();
+            entireText = new Scanner(new File(title)).useDelimiter("\\A").next();    // store entire text in the string
             words = new ArrayList<>(Arrays.asList(entireText.split(" ")));
         } catch (FileNotFoundException f) {
             System.out.println("File not found");
@@ -34,22 +25,51 @@ public class Book {
         }
     }
 
+    /**
+     * Print out all words contained in the arraylist, each on a separate line
+     */
     public void printWords() {
         words.forEach(consumer -> System.out.println(consumer));
     }
 
+    /**
+     * Get the word at the current index
+     * @return string contained at currentWordIndex of arraylist
+     */
     public String getWordAtCurrentIndex() {
         return words.get(currentWordIndex);
     }
 
+    /**
+     * Get the word at currentIndex and increment index
+     * Returns same word as getWord atCurrentIndex(), the only difference is
+     * that this method will then move on to the next word in the arraylist
+     * @return string contained at currentWordIndex of arraylist
+     * TODO: check that end of arraylist has not been reached
+     */
     public String getNextWord() {
         String nextWord = words.get(currentWordIndex);
         currentWordIndex += 1;
         return nextWord;
     }
 
+    /**
+     * Get the word contained at the previous index
+     * This method will decrement the index so that it is now viewing the previous word
+     * @return string contained at currentWordIndex after it has been decremented
+     * TODO: check that current index is not at the first element
+     */
     public String getPreviousWord() {
         currentWordIndex -= 1;
         return words.get(currentWordIndex);
+    }
+
+    public static void main(String[] args) {
+        Book b1 = new Book("text.txt");
+        System.out.println(b1.getWordAtCurrentIndex());
+        System.out.println(b1.getNextWord());
+        System.out.println(b1.getNextWord());
+        System.out.println(b1.getPreviousWord());
+        b1.printWords();
     }
 }
